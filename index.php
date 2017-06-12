aa45a4827e0d12ac0f818bf24b027c883005eed8
lingpaul <paullingertat@Pauls-MacBook-Pro.local>
2017-06-12 13:30:23 -0400

    Test commit 1

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+    <p> This converts PNG images of floorplans into 3JS walls. <p>
+
+	</body>
+</html>
